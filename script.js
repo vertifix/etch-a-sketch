6ace2df0163520a298e6e9a6b1e1bcf54c24f272
Medi <113775938+vertifix@users.noreply.github.com>
2023-04-03 10:45:28 +0200

    added clear button

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const container = document.querySelector(".container");
-gridSize = 16 * 16 + 16;
+
+userGridChoice = 34
+
+const gridDimensions = userGridChoice * userGridChoice;
+const gridSize = 600
 
 
 function makeBox() {
   
-  for (let i = 0; i < gridSize; i++) {
+  for (let i = 0; i < gridDimensions; i++) {
     
     const box = document.createElement("div");
-    box.style.backgroundColor = "red";
-    box.innerText = i;
-    box.style.width = "2em";
-    box.style.height = "2em";
+    box.style.backgroundColor = "white";
+    // box.innerText = i;
+    box.style.width = gridSize/userGridChoice+"px";
+    box.style.height = gridSize/userGridChoice+"px";
     box.className = "box";
     box.id = i;
     container.appendChild(box);
@@ -20,14 +24,22 @@ function makeBox() {
 makeBox();
 
 const pen = document.querySelectorAll(".box");
+let penColor = "green";
 
 pen.forEach (box =>
-
+  
   box.addEventListener ( "mouseenter", (e) => {
-    e.target.style.backgroundColor = "blue";
+    e.target.style.backgroundColor = penColor;
   },
   false
-)
-);
+  ));
+  
+  
+function clearScreen() {
+
+  pen.forEach (box => box.style.backgroundColor = "white");
+}
 
 
+const clearButton = document.querySelector('.clear-button');
+clearButton.addEventListener("click", clearScreen)
